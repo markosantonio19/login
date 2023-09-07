7912030fa94b4de14d66676b32649f1711e8ad51
markosantonio <1433811@senati.pe>
2023-09-07 11:26:07 -0500

    agrege boton para digite suy usuario

--- login/Form1.Designer.cs
+++ login/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(28, 51);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(79, 63);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(265, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 17);
+            label1.TabIndex = 1;
+            label1.Text = "user";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(226, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 24);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- login/Form1.cs
+++ login/Form1.cs
@@ -6,5 +6,10 @@ namespace login
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
